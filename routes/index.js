59250b0c6f2ec50d5f93de27588e8e173af88b0b
Michael Saul <michaelsaul@MacBook-Pro.local>
2020-08-27 14:17:04 -0700

    Commit for -u command

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Video Game Review Site' });
 });
 
 module.exports = router;
