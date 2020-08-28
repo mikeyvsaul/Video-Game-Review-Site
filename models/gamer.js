ef81bc7af3db4208c88f4b2030ee954ebfa54570
Michael Saul <michaelsaul@MacBook-Pro.local>
2020-08-28 10:36:09 -0700

    Add gamer model

---
+++ models/gamer.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+const gamerSchema = new Schema({
+  name: String,
+  email: String,
+  googleId: String
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Gamer', gamerSchema);
--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const methodOverride = require('method-override');
 
 require('dotenv').config();
 
 require('./config/database');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const gamersRouter = require('./routes/gamer');
 
 const app = express();
 
@@ -22,9 +23,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', gamersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
